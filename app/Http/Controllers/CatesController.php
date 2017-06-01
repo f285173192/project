a0e4f45ef4ed3c3236274701a971fbc4da79a11e
f285173192 <285173192@qq.com>
2017-06-01 17:24:28 +0800

    first commit

---
+++ app/Http/Controllers/CatesController.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Cate;
+use DB;
+
+class CatesController extends Controller
+{
+    /**
+     *显示分类列表
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        //读取分类
+        $cates = self::getCates();
+
+        //解析模板
+        return  view('admin.cate.index', ['cates'=>$cates, 'request'=>$request]);
+
+    }
+
+    /**
+     * 获取所有的分类信息 并排序
+     */
+    public static function getCates()
+    {
+        $cates = Cate::select(DB::raw('*, concat(path,",",id) as paths'))->orderBy('paths')->get();
+
+        //遍历数组 调整分类的名称  laravel   =====>  |-------laravel
+        foreach ($cates as $key => $value) {
+            //判断当前的分类是几级分类
+            $tmp = count(explode(',', $value->path)) - 1;
+            $prefix = str_repeat('|-------', $tmp);
+            $value->name = $prefix . $value->name;
+        }
+
+        return $cates;
+    }
+    /**
+     * 显示一个表单用来创建分类
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //读取所有分类
+        $cates = Cate::get();
+        return view('admin.cate.add', ['cates'=>$cates]);
+    }
+
+    /**
+     * 将分类信息 存入到数据库中
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        //如果要添加的是顶级分类  pid 和path都是0 
+        if($data['pid'] == 0 ){
+            $data['path'] = '0';
+        }else{//如果不是顶级分类
+            //读取父级分类的信息
+            $info = Cate::find($data['pid']);
+            $data['path'] = $info->path.','.$info->id;
+        }
+        //创建模型
+        $cate = new Cate;
+        $cate->name = $data['name'];
+        $cate->pid = $data['pid'];
+        $cate->path = $data['path'];
+        //
+        if($cate->save()) {
+            return redirect('/cate')->with('info', '分类添加成功');
+        }else{
+            return back()->with('info','分类添加失败');
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //读取当前分类的信息
+        $info = Cate::findOrFail($id);// 
+        //读取
+        $cates = Cate::get();
+        //解析模板
+        return view('admin.cate.edit', ['info'=>$info,'cates'=>$cates]);
+    }
+
+    /**
+     * 更新
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $cate = Cate::findOrFail($id);
+        $cate->name = $request->name;
+        $cate->pid = $request->pid;
+
+        if($cate->save()){
+            return redirect('/cate')->with('info', '分类更新成功');
+        }else{
+            return back()->with('info', '分类更新失败');
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        //删除分类
+        $cate = Cate::findOrFail($id);
+        //删除子集分类
+        $path = $cate->path . ','.$cate->id;
+        DB::table('cates')->where('path','like',$path.'%')->delete();
+        if($cate->delete()) {
+            return back()->with('info','删除成功');
+        }else{
+            return back()->with('info','删除失败');
+        }
+    }
+}
---
+++ read.md
@@ -0,0 +1,51 @@
+# 项目第一天内容
+
+1. 分析项目 找出项目的元素并进行建模 (navicat 该工具还可以导出sql语句) 建立关系
+2. 安装laravel (使用composer来安装,如果没有的话建议先安装composer)
+3. 配置虚拟主机
+4. 创建数据库设置字符集
+5. 配置laravel的数据库连接信息.
+6. 使用`artisan`命令进行模型创建.  `php artisan make:model User -m` -m会自动帮我们创建模型文件
+7. 创建表结构, 如果有关联表的话应该使用 `php artisan make:migration create_post_tag_table` 创建中间表
+8. 运行命令来生成数据表 `php artisan migrate`
+9. 创建路由规则访问网站后台.
+10. 在方法中解析模板(尽量做到前后台的一个划分). 
+11. 将目标模板中的源代码复制到指定的模板中.
+12. 将模板所需要的js, css, 图片内容复制到项目的public目录下,存放至一个目录中(admins 文件夹的名字不能跟路由名称重名).
+13. 在模板中进行路径调整, 建议大家使用asset函数进行路径调整.
+14. 调整404页面
+
+15. 可以使用工具格式化html代码  eg: http://www.sojson.com/jshtml.html
+
+16. 资源控制器
+
+17. sublime 安装 laravel artisan 插件
+18. 将代码回推 shift + tab
+
+19. 隐式控制器和resful控制器区别 (都是为了简化路由规则)
+	1. resful  1  = 7
+		隐式   1  = n
+	2. resful 会自动帮我们生成方法名 更加简洁一些. 隐式控制器则不会
+	3. 方法名 隐式控制器 需要在方法名前面添加 请求方式(get , post)
+
+20. 分类的规律
+	如果是顶级分类 则 pid 和path都是0 
+	如果不是顶级分类 则pid应该是父级分类的id, path应该是父级分类的path,父级分类的id
+
+21. ErrorException in 79833491d30ddd73c0a46bc7f5a36eae line 31: 这样的问题 解决方案
+	sublime 快捷键 ctrl+p找文件   ctrl+g  定位行数
+
+22. 方法伪造
+	<form action="/foo/bar" method="POST">
+	    <input type="hidden" name="_method" value="PUT">
+	    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+	</form>
+
+23. 中奖	
+	770991581
+	756011187
+	25638734
+
+
+
+
